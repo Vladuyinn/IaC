5988203f8c71302b30833c20c4c8698c4c4c33de
Vladuyinn <romaincrombetravail@gmail.com>
2026-03-06 11:09:38 +0100

    initial commit

---
+++ TP-local/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "tcp://localhost:2375"
+}
+
+resource "docker_image" "nginx" {
+  name         = var.image_name
+  keep_locally = true
+}
+
+resource "docker_container" "nginx" {
+  name  = var.container_name
+  image = docker_image.nginx.image_id
+  
+  ports {
+    internal = var.internal_port
+    external = var.external_port
+  }
+
+  networks_advanced {
+    name    = docker_network.app_network.name
+    aliases = ["nginx"]
+  }
+}
+
+resource "docker_network" "app_network" {
+  name = "app_network"
+}
+
+resource "docker_image" "curl" {
+  name         = "appropriate/curl:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "client" {
+  name  = "client"
+  image = docker_image.curl.image_id
+
+  # Connexion au même réseau
+  networks_advanced {
+    name = docker_network.app_network.name
+  }
+
+  # Exécution de curl vers l'alias nginx, suivi d'un sleep pour garder le conteneur en vie
+  command = ["sh", "-c", "curl -s http://nginx:80 && sleep 3600"]
+
+  # On s'assure que Nginx est démarré en premier
+  depends_on = [docker_container.nginx]
+}
+
+output "nginx_container_id" {
+  description = "L'identifiant du conteneur Nginx créé"
+  value       = docker_container.nginx.id
+}
